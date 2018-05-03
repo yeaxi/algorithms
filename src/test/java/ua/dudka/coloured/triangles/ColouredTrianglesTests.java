c899f11d432f0a1caf12d78943fd16b2030ecd9e
yeaxi <yeaxi98@gmail.com>
2018-05-03 09:37:30 +0300

    - expand test cases

--- src/test/java/ua/dudka/coloured/triangles/ColouredTrianglesTests.java
+++ src/test/java/ua/dudka/coloured/triangles/ColouredTrianglesTests.java
@@ -1,32 +1,67 @@
 package ua.dudka.coloured.triangles;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
-import java.util.*;
-
+@RunWith(Parameterized.class)
 public class ColouredTrianglesTests {
 
-    private List<List<ColouredPoint>> arguments = Arrays.asList( Arrays.asList(new ColouredPoint(new int[] {3, -4}, "blue"),  new ColouredPoint(new int[] {-7, -1}, "red"),   new ColouredPoint(new int[] {7, -6}, "yellow"),
-                                                                          new ColouredPoint(new int[] {2, 5}, "yellow"), new ColouredPoint(new int[] {1, -5}, "red"),    new ColouredPoint(new int[] {-1, 4}, "red"),
-                                                                          new ColouredPoint(new int[] {1, 7}, "red"),    new ColouredPoint(new int[] {-3, 5}, "red"),    new ColouredPoint(new int[] {-3, -5}, "blue"),
-                                                                          new ColouredPoint(new int[] {4, 1}, "blue")),
-                                                                          
-                                                           Arrays.asList( new ColouredPoint(new int[] {3, -4}, "blue"),  new ColouredPoint(new int[] {-7, -1}, "red"),   new ColouredPoint(new int[] {7, -6}, "yellow"), 
-                                                                          new ColouredPoint(new int[] {2, 5}, "yellow"), new ColouredPoint(new int[] {1, -5}, "red"),    new ColouredPoint(new int[] {1, 1}, "red"),
-                                                                          new ColouredPoint(new int[] {1, 7}, "red"),    new ColouredPoint(new int[] {1, 4}, "red"),     new ColouredPoint(new int[] {-3, -5}, "blue"),
-                                                                          new ColouredPoint(new int[] {4, 1}, "blue")),
-                                                                          
-                                                           Arrays.asList( new ColouredPoint(new int[] {1, -2}, "red"),   new ColouredPoint(new int[] {7, -6}, "yellow"), new ColouredPoint(new int[] {2, 5}, "yellow"),
-                                                                          new ColouredPoint(new int[] {1, -5}, "red"),   new ColouredPoint(new int[] {1, 1}, "red"),     new ColouredPoint(new int[] {1, 7}, "red"),
-                                                                          new ColouredPoint(new int[] {1, 4}, "red"),    new ColouredPoint(new int[] {-3, -5}, "blue"),  new ColouredPoint(new int[] {4, 1}, "blue")));
+    private List<ColouredPoint> colouredPoints;
+    private TriangleResult expectedResult;
 
-    private List<TriangleResult> expected = Arrays.asList( new TriangleResult(10, 3, 11, Arrays.asList("red"),    10),
-                                                      new TriangleResult(10, 3, 7,  Arrays.asList("red"),     6),
-                                                      new TriangleResult(9,  3, 0,  new ArrayList<String>(),  0));
+    public ColouredTrianglesTests(List<ColouredPoint> colouredPoints, TriangleResult expectedResult) {
+        this.colouredPoints = colouredPoints;
+        this.expectedResult = expectedResult;
+    }
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                        {
+                                List.of(new ColouredPoint(new int[]{3, -4}, "blue"), new ColouredPoint(new int[]{-7, -1}, "red"), new ColouredPoint(new int[]{7, -6}, "green"),
+                                        new ColouredPoint(new int[]{2, 5}, "green"), new ColouredPoint(new int[]{1, -5}, "red"), new ColouredPoint(new int[]{-1, 4}, "red"),
+                                        new ColouredPoint(new int[]{1, 7}, "red"), new ColouredPoint(new int[]{-3, 5}, "red"), new ColouredPoint(new int[]{-3, -5}, "blue"),
+                                        new ColouredPoint(new int[]{4, 1}, "blue")),
+                                new TriangleResult(10, 3, 11, List.of("red"), 10)
+                        },
+                        {
+                                List.of(new ColouredPoint(new int[]{3, -4}, "blue"), new ColouredPoint(new int[]{-7, -1}, "red"), new ColouredPoint(new int[]{7, -6}, "green"),
+                                        new ColouredPoint(new int[]{2, 5}, "green"), new ColouredPoint(new int[]{1, -5}, "red"), new ColouredPoint(new int[]{1, 1}, "red"),
+                                        new ColouredPoint(new int[]{1, 7}, "red"), new ColouredPoint(new int[]{1, 4}, "red"), new ColouredPoint(new int[]{-3, -5}, "blue"),
+                                        new ColouredPoint(new int[]{4, 1}, "blue")),
+                                new TriangleResult(10, 3, 7, List.of("red"), 6)
+                        },
+                        {
+                                List.of(new ColouredPoint(new int[]{1, -2}, "red"), new ColouredPoint(new int[]{7, -6}, "green"), new ColouredPoint(new int[]{2, 5}, "green"),
+                                        new ColouredPoint(new int[]{1, -5}, "red"), new ColouredPoint(new int[]{1, 1}, "red"), new ColouredPoint(new int[]{1, 7}, "red"),
+                                        new ColouredPoint(new int[]{1, 4}, "red"), new ColouredPoint(new int[]{-3, -5}, "blue"), new ColouredPoint(new int[]{4, 1}, "blue")),
+
+                                new TriangleResult(9, 3, 0, new ArrayList<>(), 0)
+                        },
+                        {
+                                List.of(
+                                        new ColouredPoint(new int[]{0, 0}, "red"),
+                                        new ColouredPoint(new int[]{0, 1}, "red"),
+                                        new ColouredPoint(new int[]{0, 2}, "red"),
+                                        new ColouredPoint(new int[]{1, 0}, "red")),
+                                new TriangleResult(4, 1, 3, List.of("red"), 3)
+                        }
+                }
+        );
+    }
+
+
     @Test
-    public void test_BasicTests() {
-        for (int n = 0; n < expected.size() ; n++)
-            assertEquals(expected.get(n), ColouredTriangles.countColouredTriangles( arguments.get(n) ));
+    public void test() {
+        assertEquals(expectedResult, ColouredTriangles.countColouredTriangles(colouredPoints));
     }
 }
